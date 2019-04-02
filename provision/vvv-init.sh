cb6e9a28900e66d980e558f4ac094a465b8ad24a
Crozzroads-Tomasz <38193311+tomasz-crozzroads@users.noreply.github.com>
2019-04-02 15:40:28 +0200

    Added nginx configuration

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -31,3 +31,14 @@ sed -i "s#APP_DATABASE_PREFIX=#APP_DATABASE_PREFIX=${DB_PREFIX}#" "${VVV_PATH_TO
 php bin/console db
 ## Set URL (dev)
 sed -i "s#http://localhost/slim-rest-base#${DOMAIN}#" "${VVV_PATH_TO_SITE}/config/services.dev.php"
+
+# Nginx configuration
+cp -f "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf.tmpl" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+
+if [ -n "$(type -t is_utility_installed)" ] && [ "$(type -t is_utility_installed)" = function ] && `is_utility_installed core tls-ca`; then
+    sed -i "s#{{TLS_CERT}}#ssl_certificate /vagrant/certificates/${VVV_SITE_NAME}/dev.crt;#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+    sed -i "s#{{TLS_KEY}}#ssl_certificate_key /vagrant/certificates/${VVV_SITE_NAME}/dev.key;#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+else
+    sed -i "s#{{TLS_CERT}}##" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+    sed -i "s#{{TLS_KEY}}##" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+fi
